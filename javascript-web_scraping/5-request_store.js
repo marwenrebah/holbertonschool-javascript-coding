5ffb0a11528e8b9f9f1b98e7b172f5c49cfbf128
marwenrebah <6863@holbertonstudents.com>
2024-05-14 03:49:01 -1100

    a script that gets the contents of a webpage and stores it in a file.

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
-const fs = require("fs");
-const request = require("request");
-const url = process.argv[2];
-const path = process.argv[3];
 
-request(url, function (err, data, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(path, body, "utf8", function (err) {
-      if (err) {
-        console.log(err);
-      }
-    });
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const outputFile = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
   }
+  fs.writeFile(outputFile, body, 'utf8', (err) =>{
+    if (err) {
+      console.error(err);
+    }
+  });
 });
