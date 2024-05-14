4aafcfbd7c2581af557db297b0144dd25dc1675e
marwenrebah <6863@holbertonstudents.com>
2024-05-14 03:07:37 -1100

    Star wars movie title

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, function (error, body) {
+request(url, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
