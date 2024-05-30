776582e19d9a081acb3e3707fa26266f69530055
marwenrebah <6863@holbertonstudents.com>
2024-05-30 00:54:37 -1100

    Organize a complex HTTP server using Express

---
+++ Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,12 @@
+import { AppController } from '../controllers/AppController';
+import { StudentsController } from '../controllers/StudentsController';
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', AppController.getHomepage);
+
+router.get('/students/:major?', StudentsController.getAllStudents);
+
+export default router;
