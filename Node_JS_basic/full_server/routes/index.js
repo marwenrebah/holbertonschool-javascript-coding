2120a7bc2945b0c4a126f82aaa610b43415998e0
marwenrebah <6863@holbertonstudents.com>
2024-05-30 01:20:08 -1100

    eslint fix

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -1,13 +1,11 @@
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-const express = require('express');
+const mapRoutes = (app) => {
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
 
-const router = express.Router();
-
-router.get('/', (req, res) => AppController.getHomepage(req, res));
-router.get('/students', (req, res) => StudentsController.getAllStudents(req, res));
-router.get('/students/:major', (req, res) => StudentsController.getAllStudentsByMajor(req, res));
-
-export default router;
-module.exports = router;
+export default mapRoutes;
+module.exports = mapRoutes;
